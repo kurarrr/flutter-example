55a44c8dab5d52a40353908a1729598f58fec2d7
kurarrr <y.kurarrr@gmail.com>
2020-06-03 22:19:36 +0900

    provider example

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,97 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class ChatStream {
+
+  final searchQueryController = StreamController<String>();
+  Stream<String> get query => searchQueryController.stream;
+  StreamSink<String> get changeQuery => searchQueryController.sink;
+
+  final searchResultController = StreamController<String>();
+  Stream<String> get result => searchResultController.stream;
+  StreamSink<String> get changeResult => searchResultController.sink;
+
+  ChatStream() {
+    query.listen((v) async {
+      // APIの場合は await 使う
+      final String result = "hello " + v;
+      changeResult.add(result);
+    });
+  }
+
+  void dispose() {
+    searchResultController.close();
+    searchQueryController.close();
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(          // Add the 3 lines from here... 
-        primaryColor: Colors.white,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      debugShowCheckedModeBanner: false,
+      home:
+      Scaffold(
+        body: Center(
+          child: Provider<ChatStream>(
+            create: (_) => ChatStream(),
+            dispose: (_, stream) => stream.dispose(),
+            child: MyWidget(),
+          )
+        ),
       ),
-      home: RandomWords(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
+class MyWidget extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController queryInputController = TextEditingController(text: '');
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
+    final chatStream = Provider.of<ChatStream>(context);
 
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(   // Add the lines from here... 
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {      // Add 9 lines from here...
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else { 
-            _saved.add(pair); 
-          } 
-        });
-      }, 
-    );
-  }
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(   // Add 20 lines from here...
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
+    return Column(children: <Widget>[
+      Form(
+        key: _formKey,
+        child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'write text',
                 ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-            .toList();
-          return Scaffold(         // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );        
-        },
-      ),                       // ... to here.
-    );
+                controller: queryInputController,
+              ))),
+          RaisedButton(
+            child: const Text('入力'),
+            onPressed: () =>
+              chatStream.changeQuery.add(queryInputController.text)),
+        ])),
+      StreamBuilder(
+        stream: chatStream.result,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            // エラー処理 if needed
+          }
+          if (snapshot.data != null) {
+            // snapshot.data を使ったWidgetを返す
+            return Text(snapshot.data);
+          } else {
+            // null 処理
+          }
+        }
+      )
+    ]);
   }
 }
